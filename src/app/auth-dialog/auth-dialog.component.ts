b0cce461e8dd2e01bc8f382b7e6f4b28c1a29d1a
Слава Дудин <ehot_dudin@list.ru>
2024-12-18 17:34:31 +0500

    Переделал окон авторизации

--- src/app/auth-dialog/auth-dialog.component.ts
+++ src/app/auth-dialog/auth-dialog.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-auth-dialog',
   standalone: true,
-  imports: [MatDialogModule], // Добавлен импорт MatDialogModule
+  imports: [MatDialogModule, MatButtonModule, MatIconModule], // Добавлен импорт MatDialogModule
   templateUrl: './auth-dialog.component.html',
   styleUrls: ['./auth-dialog.component.css'],
 })
